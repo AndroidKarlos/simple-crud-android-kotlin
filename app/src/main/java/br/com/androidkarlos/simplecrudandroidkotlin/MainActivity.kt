c77b025dcbbd7c57fe5cf0df527b78bb3189c671
Jose Carlos Bezerra <carlos.bezerradasilva@hotmail.com>
2021-08-24 21:56:18 -0300

    [feat] - implement recycle view

--- app/src/main/java/br/com/androidkarlos/simplecrudandroidkotlin/MainActivity.kt
+++ app/src/main/java/br/com/androidkarlos/simplecrudandroidkotlin/MainActivity.kt
@@ -2,10 +2,29 @@ package br.com.androidkarlos.simplecrudandroidkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val contacts = ArrayList<String>()
+    private lateinit var customAdapter: CustomAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var recyclerView: RecyclerView = findViewById(R.id.rvListContact)
+        customAdapter = CustomAdapter(contacts)
+        val layoutManager = LinearLayoutManager(applicationContext)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = customAdapter
+        prepareContacts()
+    }
+
+    private fun prepareContacts(){
+        contacts.add("Marinilza")
+        contacts.add("Marinaldo")
+        contacts.add("Marta")
+        customAdapter.notifyDataSetChanged()
     }
 }
